f3e725e15f9d52ba3c08c81a09ca54b0b45c4422
42randy <andystoll@proton.me>
2023-11-18 15:02:03 +0100

    commi1

--- get_next_line.c
+++ get_next_line.c
@@ -6,22 +6,86 @@
 /*   By: astoll <astoll@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 08:10:40 by astoll            #+#    #+#             */
-/*   Updated: 2023/11/16 13:34:34 by astoll           ###   ########.fr       */
+/*   Updated: 2023/11/18 15:00:18 by astoll           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char *get_next_line(int fd)
+static char	*read_and_save(int fd, char *buf, char *backup)
 {
-	static	t_list	*stash;
-	char			*line;
-	
-	stash = NULL;
-	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, &line, 0) < 0)//read 0 pour tester si le fichier est lisible, -1 si ya probleme ac read
+	int		read_line;
+	char	*temp;
+
+	read_line = 1;
+	while (read_line != '\0')
+	{
+		read_line = read(fd, buf, BUFFER_SIZE);
+		if (read_line == -1)
+		{
+			return (0);
+		}
+		else if (read_line == 0)
+			break ;
+		buf[read_line] = '\0';
+		if (!backup)
+			backup = ft_strdup("");
+		temp = backup;
+		backup = ft_strjoin(temp, buf);
+		free(temp);
+		temp = NULL;
+		if (ft_strchr(buf, '\n'))
+			break ;
+	}
+	return (backup);
+}
+
+static char	*extract(char *line)
+{
+	size_t	count;
+	char	*backup;
+
+	count = 0;
+	while (line[count] != '\n' && line[count] != '\0')
+	{
+		count++;
+	}
+	if (line[count] == '\0' || line[1] == '\0')
+	{
+		return (0);
+	}
+	backup = ft_substr(line, count + 1, ft_strlen(line) - count);
+	if (*backup == '\0')
+	{
+		free(backup);
+		backup = NULL;
+	}
+	line[count + 1] = '\0';
+	return (backup);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*line;
+	char		*buf;
+	static char	*backup;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+	{
+		return (0);
+	}
+	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buf)
+	{
+		return (0);
+	}
+	line = read_and_save(fd, buf, backup);
+	free(buf);
+	buf = NULL;
+	if (!line)
 	{
 		return (NULL);
 	}
-	line = NULL;
-	read_and_stash(fd, &stash);
+	backup = extract(line);
+	return (line);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,9 +6,107 @@
 /*   By: astoll <astoll@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 08:10:33 by astoll            #+#    #+#             */
-/*   Updated: 2023/11/16 08:45:43 by astoll           ###   ########.fr       */
+/*   Updated: 2023/11/18 14:18:50 by astoll           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+int	ft_strlen(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	while ((char)c != *s)
+	{
+		if (!*s)
+		{
+			return (0);
+		}
+		s++;
+	}
+	return ((char *)s);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	char	*dest;
+	int		i;
+
+	dest = malloc(sizeof(char) * (ft_strlen(s1) + 1));
+	i = 0;
+	if (!dest)
+	{
+		return (0);
+	}
+	while (s1[i] != '\0')
+	{
+		dest[i] = s1[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int		i;
+	int		j;
+	char	*dst;
+
+	i = 0;
+	j = 0;
+	dst = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!dst)
+	{
+		return (0);
+	}
+	while (s1[i] != '\0')
+	{
+		dst[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		dst[i + j] = s2[j];
+		j++;
+	}
+	dst[i + j] = '\0';
+	return (dst);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	slen;
+	char	*str;
+	size_t	i;
+
+	if (!s)
+		return (0);
+	slen = ft_strlen(s);
+	if (slen < start)
+		return (ft_strdup(""));
+	slen = slen - start;
+	if (slen < len)
+		len = slen;
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (0);
+	i = 0;
+	while (s[i + start] && i < len)
+	{
+		str[i] = s[i + start];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
